baa691e0a19d00b09ce0acd6033644c585ecdfd6
VyLNB <goumao1906@gmail.com>
2026-05-15 12:07:50 +0700

    Render project links individually on detail page

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,12 +1,12 @@
 export const projects = [
     {
         id: "1",
-        title: "SyncChat - Real-time Messaging Web Application",
+        title: "Real-time Messaging Web Application",
         type: "Full-stack",
         techStack: ["ReactJS", "Node.js (ExpressJS)", "MongoDB", "Socket.io", "Postman", "JWT"],
         description: "Ứng dụng nhắn tin đa nền tảng cho phép người dùng gửi tin nhắn văn bản, hình ảnh, tệp đính kèm và thực hiện cuộc gọi thoại với giao diện hiện đại, tối ưu.\n\nTính năng nổi bật: tin nhắn real-time, tạo nhóm chat, trạng thái online/offline, chia sẻ file đa định dạng và mã hóa bảo mật đầu cuối.\n\nỨng dụng giúp kết nối mọi người mọi lúc mọi nơi một cách nhanh chóng và bảo mật.",
         linkGithub: "https://github.com/VyLNB/Real-time-chat",
-        linkDemo: "https://realtime-chat-60277.web.app/"
+        linkDemo: "https://real-time-chat-blond.vercel.app"
     },
     {
         id: "2",
@@ -14,8 +14,8 @@ export const projects = [
         type: "Frontend",
         techStack: ["ReactJS", "Tailwind CSS"],
         description: "Ứng dụng cung cấp một Dashboard quản trị giúp theo dõi toàn diện hoạt động kinh doanh với các chỉ số như khách hàng, đơn hàng và doanh thu. Hệ thống sử dụng biểu đồ trực quan (Recharts) để phân tích dữ liệu theo thời gian, hiển thị doanh thu theo tháng, trạng thái đơn hàng và danh sách sản phẩm bán chạy, giúp người quản trị dễ dàng đưa ra quyết định.\n\nBên cạnh đó, hệ thống hỗ trợ đầy đủ các chức năng quản lý như sản phẩm, đơn hàng, người dùng, danh mục, thương hiệu và mã giảm giá. Admin có thể thêm, chỉnh sửa, tìm kiếm dữ liệu và theo dõi chi tiết từng đối tượng (ví dụ: thông tin đơn hàng, khách hàng, sản phẩm). Điều này giúp tối ưu quy trình vận hành và quản lý dữ liệu tập trung.\n\nNgoài ra, ứng dụng tích hợp xác thực và bảo mật với cơ chế đăng nhập, phân quyền truy cập và bảo vệ route, đảm bảo chỉ người dùng hợp lệ mới có thể truy cập hệ thống quản trị. Admin cũng có thể quản lý thông tin cá nhân thông qua trang hồ sơ riêng.",
-        linkGithub: "https://github.com/VyLNB/E-Commerce-NodeJS",
-        linkDemo: "https://e-commerce-website-d2c08.web.app/"
+        linkGithub: "https://github.com/VyLNB/E-Commerce-NodeJS"
+        // linkDemo: "https://e-commerce-website-d2c08.web.app/"
     },
     {
         id: "3",
@@ -23,7 +23,7 @@ export const projects = [
         type: "Full-stack",
         techStack: ["ReactJS", "TypeScript", "Tailwind CSS", "Postman", "NodeJS (ExpressJS)", "MongoDB", "Kanban"],
         description: "Ứng dụng quản lý công việc cá nhân cho phép người dùng theo dõi và quản lý công việc một cách hiệu quả. Với giao diện trực quan và dễ sử dụng, ứng dụng hỗ trợ tạo, cập nhật, xóa và phân loại công việc, giúp người dùng sắp xếp công việc hàng ngày một cách khoa học.",
-        linkGithub: "https://github.com/VyLNB/Task-Manager",
-        linkDemo: "https://e-commerce-website-d2c08.web.app/"
+        linkGithub: "https://github.com/VyLNB/Task-Manager"
+        // linkDemo: "https://e-commerce-website-d2c08.web.app/"
     }
 ]
